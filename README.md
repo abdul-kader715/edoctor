704766a0749eec57400ace4dabe6f969108b599f
abdul-kader715 <limonislam715@gmail.com>
2021-10-27 17:21:45 +0600

    new file add

--- README.md
+++
@@ -1,2 +0,0 @@
-# edoctor
-new
---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".notification").click(function(){
+        $(".notification-item").toggleClass("notification-active");
+    });
+
+    $(".menu-btn,.close-sidebar").click(function(){
+        $(".left-sidebar").toggleClass("left-sidebar-active");
+    });
+
+});
